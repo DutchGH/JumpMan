43734cd967311858afdbaeedcabcb0f4d0188100
Jacob Holland <sc15j3h@leeds.ac.uk>
2016-04-01 19:46:30 +0100

    Setting up Eclipse IDE for more 'controlled' development

---
+++ cw2/src/sdl_project.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : sdl_project.c
+ Author      : Jacob Holland
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Main file for execution of program for SDL
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
