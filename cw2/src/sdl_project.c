5adb63555ab721549581834257766a56e8610a26
Jacob Holland <sc15j3h@leeds.ac.uk>
2016-04-18 20:55:46 +0100

    Allowed Images to Load

--- cw2/src/sdl_project.c
+++ cw2/src/sdl_project.c
@@ -11,39 +11,90 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+//window to render to
+SDL_Window* gWindow = NULL;
+//surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+//image to load on the screen
+SDL_Surface* gRequiem = NULL;
 
-int main(int argc, char* argv[])
+bool init()
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
-
-	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	bool success = true;
+	if(SDL_Init(SDL_INIT_VIDEO) <0)
 	{
-		printf("SDL could not initialise. SDL Error: %s\n", SDL_GetError());
+		printf("SDL could not initialise - SDL Error: %s\n", SDL_GetError());
 	}
 	else
 	{
-		window = SDL_CreateWindow("Promethian Knights", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if(window == NULL)
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if(gWindow == NULL)
 		{
-			printf("window could not be created - SDL Error: %s\n", SDL_GetError());
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
 		}
 		else
 		{
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
+}
+
+
+bool loadMedia()
+{
+	bool success = true;
+	gRequiem = IMG_Load("gfx/requiem.jpg");
+	if(gRequiem == NULL)
+	{
+		printf("unable to load image %s - SDL Error: %s\n", "gfx/requiem.jpg",SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void quit()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gRequiem);
+	gRequiem = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+	if(!init())
+	{
+		printf("Failed To Initialise! \n");
+	}
+	else
+	{
+		if(!loadMedia())
+		{
+			printf("Failed to load image!\n");
+		}
+		else
+		{
+			SDL_BlitSurface(gRequiem, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
 			SDL_Delay(2000);
 		}
 	}
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-
+	quit();
 	exit(0);
 }
