ce709c38ae9fe5b00bcfaa474725354f2412bd1b
Jacob Holland <sc15j3h@leeds.ac.uk>
2016-04-18 21:59:49 +0100

    created event loop so user can manually exit program. -

--- cw2/src/sdl_project.c
+++ cw2/src/sdl_project.c
@@ -12,8 +12,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
@@ -24,20 +25,25 @@ SDL_Surface* gScreenSurface = NULL;
 //image to load on the screen
 SDL_Surface* gRequiem = NULL;
 
+//Starts SDL & creates a window
 bool init()
 {
+	//flag for initialisation
 	bool success = true;
+
+	//initialise SDL
 	if(SDL_Init(SDL_INIT_VIDEO) <0)
 	{
 		printf("SDL could not initialise - SDL Error: %s\n", SDL_GetError());
+		success = false;
 	}
 	else
 	{
 		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		gWindow = SDL_CreateWindow("Promethian Knights", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if(gWindow == NULL)
 		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			printf( "Window could not be created -  SDL_Error: %s\n", SDL_GetError() );
 			success = false;
 		}
 		else
@@ -49,10 +55,12 @@ bool init()
 	return success;
 }
 
-
 bool loadMedia()
 {
+	//success flag
 	bool success = true;
+
+	//load splash image
 	gRequiem = IMG_Load("gfx/requiem.jpg");
 	if(gRequiem == NULL)
 	{
@@ -78,22 +86,37 @@ void quit()
 
 int main(int argc, char* argv[])
 {
+	//Start SDL & create window
 	if(!init())
 	{
 		printf("Failed To Initialise! \n");
 	}
 	else
 	{
+		//load Images
 		if(!loadMedia())
 		{
 			printf("Failed to load image!\n");
 		}
 		else
 		{
-			SDL_BlitSurface(gRequiem, NULL, gScreenSurface, NULL);
-			SDL_UpdateWindowSurface(gWindow);
-			SDL_Delay(2000);
+			bool go = true;
+			SDL_Event e;
+			while(go)
+			{
+				while(SDL_PollEvent(&e))
+				{
+					if(e.type == SDL_QUIT)
+					{
+						go = false;
+					}
+				}
+				SDL_BlitSurface(gRequiem, NULL, gScreenSurface, NULL);
+				SDL_UpdateWindowSurface(gWindow);
+			}
+
 		}
+
 	}
 	quit();
 	exit(0);
